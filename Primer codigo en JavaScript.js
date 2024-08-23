7e61cb46121a1180c75a586183c896c93f0fd93e
unknown <nicolis1415@gmail.com>
2024-08-23 13:38:19 -0500

    Implementacion de primer codigo y creacion de repositorio

---
+++ Primer codigo en JavaScript.js
@@ -0,0 +1,27 @@
+
+
+//Este es un comentario en JavaScript
+/*
+Esta es otra manera de realizar
+un comentario en JavaScript
+*/ 
+
+console.log("Hola mundo")
+console.log("Hola mundo"," perros hijos de us puta madre")
+
+
+var persona;
+persona = "Nicolas";
+console.log(persona);
+
+var petDog = "Rex";
+var petCat = "Popper";
+console.log("El nombre de mi perra es: "+petDog);
+console.log("El nombre de mi gata es: "+petCat);
+var catSound = "purr";
+var dogSound = "woof";
+
+
+var score = 100
+var  boleano = score == "100" 
+console.log(boleano)
