ed341f3ab09db473983554470913946359f48830
unknown <nicolis1415@gmail.com>
2024-09-04 17:16:54 -0500

    metodos y arreglos

---
+++ metodos forEach.js
@@ -0,0 +1,48 @@
+const veggies = ['onion', 'garlic', 'potato'];
+veggies.forEach( function(veggie, index) {
+    console.log(`${index}. ${veggie}`);
+});
+console.log("*******************************************************")
+const fruits = ['kiwi','mango','apple','pear'];
+function appendIndex(fruit, index) {
+    console.log(`${index}. ${fruit}`)
+}
+fruits.forEach(appendIndex);
+console.log("*******************************************************")
+
+const nums = [0,10,20,30,40,50];
+nuevo = nums.filter( function(num) {
+    return num > 20;
+})
+
+console.log(nuevo)
+
+const nums2 = [0,10,20,30,40,50];
+nuevo1 =nums.map( function(num) {
+    return num / 10
+})
+
+console.log(nuevo1)
+
+const result = [];
+const drone = {
+    speed: 100,
+    color: 'yellow'
+}
+const droneKeys = Object.keys(drone);
+droneKeys.forEach( function(key) {
+    result.push(key, drone[key])
+})
+console.log(result)
+
+let bestBoxers = new Map();
+bestBoxers.set(1, "The Champion");
+bestBoxers.set(2, "The Runner-up");
+bestBoxers.set(3, "The third place");
+
+console.log(bestBoxers);
+
+const repetitiveFruits = ['apple','pear','apple','pear','plum', 'apple'];
+const uniqueFruits = new Set(repetitiveFruits);
+console.log(uniqueFruits);
+
